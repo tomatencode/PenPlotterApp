95152b4cb270fdcd0b2bdc9ac74ad652a93ffb92
tomatencode <ingo_thomas@gmx.de>
2026-04-11 18:32:54 +0200

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // enables class-based dark mode
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Tailwind scans React files here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
