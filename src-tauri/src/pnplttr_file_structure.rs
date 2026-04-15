c976991ff6baa70956f3150d10f2d10346830352
tomatencode <ingo_thomas@gmx.de>
2026-04-15 16:39:13 +0200

    new doc structure

--- src-tauri/src/pnplttr_file_structure.rs
+++ src-tauri/src/pnplttr_file_structure.rs
@@ -16,9 +16,38 @@ pub struct MetaSettings {
 }
 
 #[derive(Serialize, Deserialize, Clone)]
+pub struct Pen {
+    pub name: String,
+    pub color: String,
+    pub width: f64,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(tag = "type")]
+pub enum Element {
+    Line {
+        x1: f64,
+        y1: f64,
+        x2: f64,
+        y2: f64,
+    },
+    Rect {
+        x: f64,
+        y: f64,
+        w: f64,
+        h: f64,
+    },
+    Circle {
+        cx: f64,
+        cy: f64,
+        r: f64,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Layer {
     pub name: String,
-    pub penid: String,
+    pub pen: Pen,
     pub elements: Vec<Element>,
 }
 
@@ -45,50 +74,4 @@ impl PnplttrDocument {
             layers: vec![],
         }
     }
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-#[serde(tag = "type")]
-pub enum Element {
-    Line {
-        id: String,
-        pen: String,
-        z: i32,
-        properties: LineProps,
-    },
-    Rect {
-        id: String,
-        pen: String,
-        z: i32,
-        properties: RectProps,
-    },
-    Circle {
-        id: String,
-        pen: String,
-        z: i32,
-        properties: CircleProps,
-    },
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct LineProps {
-    pub x1: f64,
-    pub y1: f64,
-    pub x2: f64,
-    pub y2: f64,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct RectProps {
-    pub x: f64,
-    pub y: f64,
-    pub w: f64,
-    pub h: f64,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct CircleProps {
-    pub cx: f64,
-    pub cy: f64,
-    pub r: f64,
 }
